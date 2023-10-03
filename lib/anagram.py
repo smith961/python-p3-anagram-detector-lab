f546ab0ae45ea6ff9db66f97a9be484d7234d2c3
smith <smithbabawande@gmail.com>
2023-10-04 00:24:38 +0100

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, letters):
+        anagrams = []
+        for letter in letters:
+            if sorted(letter.lower()) == self.sorted_word:
+                anagrams.append(letter)
+        return anagrams
